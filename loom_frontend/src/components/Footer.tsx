f52d38e60f6e2de16c6e2a40afecb144717a1b11
Garv Mathur <garvmathur7700@gmail.com>
2025-10-08 15:32:45 +0530

    added footer for dev cred

---
+++ loom_frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import '../App.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-left">
+        <span className="footer-text">Made with ❤️ by Garv Mathur</span>
+      </div>
+      <div className="footer-right">
+        <span className="footer-info">Connected users will see changes in real-time</span>
+      </div>
+    </footer>
+  );
+}
