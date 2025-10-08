427180ede61332abdc8c8d5ac6fec8d419ab61a6
Garv Mathur <garvmathur7700@gmail.com>
2025-10-08 15:35:38 +0530

    minor modification

--- loom_frontend/src/main.tsx
+++ loom_frontend/src/main.tsx
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Header from './components/header.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <>
-    <Header />
-    <App />
-  </>
+  <App />
 );
